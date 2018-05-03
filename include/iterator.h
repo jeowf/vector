1a81529002193406469f20262909b147a2cb7152
Alison Hedigliranes da Silva <lalodbzkin@A202-02L.imd.local>
2018-05-03 14:37:13 -0300

    Comentários doxigen e makefile

--- include/iterator.h
+++ include/iterator.h
@@ -1,33 +1,106 @@
 #ifndef ITERATOR
 #define ITERATOR
 
+    /*! @namespace sc
+    *
+    * @brief Scope of Vector
+    */
 namespace sc{
-
+ /*! @tparam T
+    *
+    * @brief Template to class MyIterator
+    */
 	template <typename T>
+   /*! @class MyIterator
+     *
+     * @brief A class that manage the acess the elements in a list
+    */
     class MyIterator { 
     public: 
-
+        /*! @typedef ref
+        *
+        * @brief Replace all incidences of type MyIterator& to ref
+        */
         typedef MyIterator& ref ;
+
+        /*! 
+        * @brief MyIterador Constructor
+        *
+        * Initializes the iterator with null pointer  
+        *
+        * @param nullptr null pointer
+        */
 
         MyIterator(void); 
 
+        /*! 
+        * @brief MyIterador Constructor
+        *
+        * Initializes the iterator with non-null pointer  
+        *
+        * @param ptr non-null pointer
+        */
+
         MyIterator(T*); 
-        
-        const T& operator* (void) const; 
+        /*! 
+        * @brief '*' Overload
+        *
+        * Overload '*' operator to take the current element in list
+        *
+        * @return Return a reference to the object in the position poited by the iterator
+        */
+        const T& operator* (void) const;
+        /*! 
+        * @brief '++' Overload
+        *
+        * Overload '++' operator to advances iterator to the next location within the list
+        *
+        * @return Return a reference to the object in the position poited by the iterator
+        */ 
         MyIterator & operator++ (void);  // ++it; 
-
+        /*! 
+        * @brief '++' Overload
+        *
+        * Overload '++' operator to return the current element in the list and advances iterator to the next location within the list
+        *
+        * @return Return a reference to the object in the previous position poited by the iterator
+        */ 
         MyIterator operator++ (int); // it++; 
-
+        /*! 
+        * @brief '--' Overload
+        *
+        * Overload '--'operator to back iterator to the previous location within the list
+        *
+        * @return Return a reference to the object in the position poited by the iterator
+        */ 
         MyIterator & operator-- (void); // --it;
-
+        /*! 
+        * @brief '--' Overload
+        *
+        * Overload '--' operator to return the current element in the list and back iterator to the previous location within the list
+        *
+        * @return Return a reference to the object in the previous position poited by the iterator
+        */ 
         MyIterator operator-- (int); // it--; 
-
+        /*! 
+        * @brief '==' Overload
+        *
+        * Overload '==' operator to verify if two iterators points to same localization in the list
+        *
+        * @return Return true if two iterators points to same localization in the list
+        */ 
         bool operator== (const MyIterator &) const; 
-
+        /*! 
+        * @brief '!=' Overload
+        *
+        * Overload '!=' operator to verify if two iterators points to differentes localization in the list
+        *
+        * @return Return true if two iterators points to differentes localization in the list
+        */ 
         bool operator!= (const MyIterator &) const; 
 
     private: 
-        T *current; 
+        T *current; /*!< Variable to storage the current element in the list */ 
     };
 }
 
--- include/iterator.inl
+++ include/iterator.inl
@@ -1,23 +1,26 @@
 #include <iterator.h>
 
 namespace sc {
-
+	//construtor inicado com ponteiro nulo
 	template <typename T>
 	MyIterator<T>::MyIterator(void) : current(nullptr){ }
-
+	
+	//construtor inicado com ponteiro não nulo
 	template <typename T>
 	MyIterator<T>::MyIterator(T* ptr) : current(ptr){ }
 
+	//operador * sobrecarregado para pegar o valor do elemento atual da lista
 	template <typename T>
 	const T& MyIterator<T>::operator*( void ) const{
 		return *current;
 	}
-
+	//operador ++ sobrecarregado para pegar o valor do  próximo elemento da lista
 	template <typename T>
 	MyIterator<T> & MyIterator<T>::operator++ (void){
 		return ++current;
 	}
-
+	//operador ++ sobrecarregado para pegar o valor do elemento atual da lista e em seguida
+	//passar o iterador para o próximo elemento da lista
 	template <typename T>
 	MyIterator<T> MyIterator<T>::operator++ ( int ){
 		MyIterator temp( *this );
@@ -25,12 +28,13 @@ namespace sc {
         return temp;
 
 	}
-
+	//operador -- sobrecarregado para pegar o valor do elemento anterior da lista
 	template <typename T>
 	MyIterator<T> & MyIterator<T>::operator-- (void){
 		return --current;
 	}
-
+	//operador -- sobrecarregado para pegar o valor do elemento atual da lista e em seguida
+	//passar o iterador para o elemento anterior da lista
 	template <typename T>
 	MyIterator<T> MyIterator<T>::operator-- ( int ){
 		MyIterator temp( *this );
@@ -38,12 +42,12 @@ namespace sc {
         return temp;
 
 	}
-
+	//operador == sobrecarregado para verificar se dois iteradores apontam para o mesmo elemento
 	template <typename T>
 	bool MyIterator<T>::operator==( const MyIterator& rhs ) const {
 	    return current == rhs.current;
 	}
-
+	//operador == sobrecarregado para verificar se dois iteradores não apontam para o mesmo elemento
 	template <typename T>
 	bool MyIterator<T>::operator!=( const MyIterator& rhs ) const {
 	    return current != rhs.current;
